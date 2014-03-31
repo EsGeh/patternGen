645b2dc804bcfed83b3c7fe70c84b067c2097672
Samuel Gfrörer <SamuelGfroerer@googlemail.com>
2014-04-01 01:45:10 +0200

    basic io settings for the server

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
+import ProgramTypes
 import ParseArgs
 
---import Prelude hiding( Left, Right, Either)
 import System.Environment
 import Network
+import System.IO
 
 
 main = do
@@ -12,10 +13,35 @@ main = do
 	let settings = parseArgs args
 	case settings of
 		Left error -> fail $ "error while reading options: \n" ++ error
-		Right settings -> putStr $ "parameters parsed: " ++ show settings
-	putStrLn "starting server..."
-	serve settings
+		Right settings -> do
+			putStrLn "starting server..."
+			ioSettings <- initIOSettings settings
+			serve ioSettings
 	return ()
 
-serve settings = return ()
+serve IOSettings{ get=get, put=put } = do
+	put "Hallo"
+	return ()
 
+initIOSettings settings = do
+	case port settings of
+		Nothing -> return $ IOSettings {
+			get = getLine,
+			put = putStrLn
+		}
+		Just port -> do
+			handle <- initTCPServer port
+			return $ IOSettings {
+				get = hGetLine handle,
+				put = hPutStrLn handle
+			}
+initTCPServer port = withSocketsDo $ do
+	sock <- listenOn $ PortNumber $ fromIntegral port
+	(handle, _, _) <- accept sock
+	hSetBuffering handle NoBuffering
+	return handle
+
+data IOSettings = IOSettings {
+	get :: IO String,
+	put :: String -> IO ()
+}
